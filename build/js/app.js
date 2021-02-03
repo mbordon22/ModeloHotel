7bed89784424790468d354ab606e49e1632e186e
mbordon22 <rivadenneira@gmail.com>
2021-02-03 19:03:12 -0300

    icono sol girando

--- build/js/app.js
+++ build/js/app.js
@@ -1,11 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(){
-    if(window.screen.width > 480){
-        document.querySelector(".navegacion-principal").classList.add("show");
-    }
+    
 });
+
 
 document.querySelector(".menu-toggle a").addEventListener("click", function(e){
     e.preventDefault();
     document.querySelector(".navegacion-principal").classList.toggle("show");
     
 });
+
+
+if(window.screen.width > 480){
+    document.querySelector(".navegacion-principal").classList.add("show");
+}
