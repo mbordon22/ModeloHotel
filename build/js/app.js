d55aed400abc0a839e8247d5a93a79d72727bc08
mbordon22 <rivadenneira@gmail.com>
2021-02-03 18:56:29 -0300

    cambios del header

--- build/js/app.js
+++ build/js/app.js
@@ -1,13 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(){
-    
+    if(window.screen.width > 480){
+        document.querySelector(".navegacion-principal").classList.add("show");
+    }
 });
 
 document.querySelector(".menu-toggle a").addEventListener("click", function(e){
-
     e.preventDefault();
-    if(document.querySelector(".navegacion-principal").classList.contains("show")){
-        document.querySelector(".navegacion-principal").classList.remove("show");    
-    }else{
-        document.querySelector(".navegacion-principal").classList.add("show");
-    }
+    document.querySelector(".navegacion-principal").classList.toggle("show");
+    
 });
