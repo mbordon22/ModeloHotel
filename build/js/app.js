87f7b590b4e3c0716f3eab2b31678306129df746
mbordon22 <rivadenneira@gmail.com>
2021-02-03 22:29:05 -0300

    Update app.js

--- build/js/app.js
+++ build/js/app.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){
-    
+    maquinaEscribir();
 });
 
 
@@ -12,4 +12,16 @@ document.querySelector(".menu-toggle a").addEventListener("click", function(e){
 
 if(window.screen.width > 480){
     document.querySelector(".navegacion-principal").classList.add("show");
+}
+
+
+var i = 0,
+    texto = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s";
+
+function maquinaEscribir(){
+    if(i < texto.length){
+        document.querySelector("#texto-bienvenida").innerHTML += texto.charAt(i);
+        i++;
+        setTimeout(maquinaEscribir, 200);
+    }
 }
