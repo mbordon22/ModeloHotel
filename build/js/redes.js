475fa88133a7e2f1b4e2a5be97880394afe1651b
mbordon22 <rivadenneira@gmail.com>
2021-02-08 11:23:43 -0300

    cambio color a iconos redes sociales

--- build/js/redes.js
+++ build/js/redes.js
@@ -1,5 +1,25 @@
 document.addEventListener("DOMContentLoaded", () =>{
-    console.log(window.screenY);
+    document.addEventListener("scroll", ()=>{
+        console.log(window.scrollY);
+        
+        if(window.scrollY < 500){
+            var iconos = document.querySelectorAll(".redes-sociales i");
+            for(var i = 0; i < iconos.length; i++){
+                iconos[i].classList.remove("color");
+            }
+        }
+        else if(window.scrollY >= 500){
+            var iconos = document.querySelectorAll(".redes-sociales i");
+            for(var i = 0; i < iconos.length; i++){
+                iconos[i].classList.add("color");
+            }
+        }
+        if(window.scrollY >= 1595){
+            var iconos = document.querySelectorAll(".redes-sociales i");
+            for(var i = 0; i < iconos.length; i++){
+                iconos[i].classList.remove("color");
+            }
+        }
+    })
 });
 
-console.log(window.scroll);
