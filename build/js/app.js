3a9860f50450ca30c7647e2258ab3a29a6c45be8
mbordon22 <rivadenneira@gmail.com>
2021-02-08 21:29:59 -0300

    pagina de habitaciones

--- build/js/app.js
+++ build/js/app.js
@@ -3,6 +3,9 @@ document.addEventListener("DOMContentLoaded", function () {
     if(document.querySelector(".home-page")){
         maquinaEscribir();
     }
+
+
+    cargarImagenes();
     
 });
 
@@ -40,3 +43,61 @@ window.addEventListener("load", () => {
     loader.style.opacity = 0;
     loader.style.visibility = "hidden";
 })
+
+
+//Funcion cargar imagenes
+function cargarImagenes() {
+    const galeria = document.querySelector(".galeria-imagenes");
+
+    for (let i = 1; i <= 8; i++) {
+        const imagen = document.createElement("IMG");
+        imagen.src = `build/img/habitaciones/${i}.webp`;
+        imagen.dataset.imagenId = i;
+
+        //Añadir la funcion de mostrar imagen
+        imagen.onclick = mostrarImagen;
+
+        const lista = document.createElement("LI");
+
+        lista.appendChild(imagen);
+
+        galeria.appendChild(lista);
+    }
+}
+function mostrarImagen(e) {
+    const id = parseInt(e.target.dataset.imagenId);
+
+    //Generar la imagen
+    const imagen = document.createElement("IMG");
+    imagen.src = `build/img/habitaciones/${id}.webp`;
+    imagen.style.width = "800px";
+    imagen.style.height = "500px";
+
+    const overlay = document.createElement("DIV");
+    overlay.appendChild(imagen);
+    overlay.classList.add("overlay");
+
+    //Boton cerrar imagen
+    const cerrarImagen = document.createElement("P");
+    cerrarImagen.textContent = "X";
+    cerrarImagen.classList.add("btn-cerrar");
+
+    //Cuando se cierra la imagen
+    overlay.onclick = function () {
+        overlay.remove();
+        body.classList.remove("fijar-body");
+    }
+
+    //Boton se cierra la imagen
+    cerrarImagen.onclick = function () {
+        overlay.remove();
+        body.classList.remove("fijar-body");
+    }
+
+    overlay.appendChild(cerrarImagen);
+
+    //Mostrar en el HTML
+    const body = document.querySelector("body");
+    body.appendChild(overlay);
+    body.classList.add("fijar-body");
+}
