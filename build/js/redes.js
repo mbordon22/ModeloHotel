1bd725cb679174121d045d3a6cd7a4dca951b801
rivadenneira <rivadenneira@gmail.com>
2022-02-09 17:09:22 -0300

    cambios en tamaños de fuentes

--- build/js/redes.js
+++ build/js/redes.js
@@ -1,46 +1,15 @@
-document.addEventListener("DOMContentLoaded", () =>{
-        if(document.querySelector(".home-page")){
-            document.addEventListener("scroll", ()=>{
-                menuFijo();
-                colorRedes();
-            });
-        }
-        else{
-            var iconos = document.querySelectorAll(".redes-sociales i");
-            for(var i = 0; i < iconos.length; i++){
-                iconos[i].classList.add("color");
-            }
-        }
-    
+document.addEventListener("DOMContentLoaded", () => {
+  if (document.querySelector(".home-page")) {
+    document.addEventListener("scroll", () => {
+      menuFijo();
+    });
+  }
 });
 
-function colorRedes(){
-
-    if(window.scrollY < 500){
-        var iconos = document.querySelectorAll(".redes-sociales i");
-        for(var i = 0; i < iconos.length; i++){
-            iconos[i].classList.remove("color");
-        }
-    }
-    else if(window.scrollY >= 500){
-        var iconos = document.querySelectorAll(".redes-sociales i");
-        for(var i = 0; i < iconos.length; i++){
-            iconos[i].classList.add("color");
-        }
-    }
-    if(window.scrollY >= 1595){
-        var iconos = document.querySelectorAll(".redes-sociales i");
-        for(var i = 0; i < iconos.length; i++){
-            iconos[i].classList.remove("color");
-        }
-    }
-}
-
-function menuFijo(){
-    
-    if(window.scrollY >= 100){
-        document.querySelector(".barra").classList.add("fijo");
-    }else{
-        document.querySelector(".barra").classList.remove("fijo");
-    }
+function menuFijo() {
+  if (window.scrollY >= 100) {
+    document.querySelector(".barra").classList.add("fijo");
+  } else {
+    document.querySelector(".barra").classList.remove("fijo");
+  }
 }
